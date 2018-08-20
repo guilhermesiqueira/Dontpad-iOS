74d4eae06f767194441da2cdbeeea1e61e6c2b02
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-08-20 09:37:03 -0300

    Add icon and storyboard

--- Dontpad-iOS/TextAreaController.swift
+++ Dontpad-iOS/TextAreaController.swift
@@ -6,4 +6,125 @@
 //  Copyright © 2018 Djorkaeff Alexandre Vilela Pereira. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TextAreaController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var textArea: UITextView!
+    var dataReceived = String()
+    var dataTextArea = String()
+    
+    @IBAction func postAction(_ sender: UIButton) {
+        var urlEntry = labelTitle.text
+        urlEntry = "http://www.dontpad.com/" + urlEntry!
+        guard let url = URL(string: urlEntry!) else { return; }
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        var postString = "id=text&text="
+        postString += self.textArea.text!
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else { // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 { // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+        }
+        task.resume()
+    }
+    
+    @IBAction func getAction(_ sender: Any) {
+        var urlEntry = labelTitle.text
+        urlEntry = "http://www.dontpad.com/" + urlEntry!
+        
+        guard let url = URL(string: urlEntry!) else { return; }
+        
+        let session = URLSession.shared.dataTask(with: url) { (data, response, poop) in
+            if let data = data {
+                self.dataTextArea = self.getTextArea(data: data)
+                DispatchQueue.main.async {
+                    self.textArea.text = self.dataTextArea
+                }
+            }
+            
+        }
+        session.resume()
+    }
+    
+    func post() {
+        var urlEntry = labelTitle.text
+        urlEntry = "http://www.dontpad.com/" + urlEntry!
+        guard let url = URL(string: urlEntry!) else { return; }
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        var postString = "id=text&text="
+        postString += self.textArea.text!
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+        }
+        task.resume()
+    }
+    
+    func recursivaQueue() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
+            self.recursivaQueue()
+            self.post()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textArea.delegate = self
+        self.labelTitle.text = self.dataReceived
+        self.textArea.text = dataTextArea
+        getAction(NSNull.self)
+        recursivaQueue()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func getTextArea(data: Data) -> String {
+        
+        let strData = String(data: data, encoding: String.Encoding.utf8) as String?
+        var string = strData?.replacingOccurrences(of: "<html lang=\"en\">(.|\n)*?<textarea id=\"text\">", with: "", options: .regularExpression, range: nil)
+        
+        string = string?.replacingOccurrences(of: "</textarea>(.|\n)*?</html>", with: "", options: .regularExpression, range: nil)
+        
+        return string!;
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textArea.resignFirstResponder()
+        return true
+    }
+}
--- Dontpad-iOS/ViewController.swift
+++ Dontpad-iOS/ViewController.swift
@@ -1,25 +1,68 @@
 //
 //  ViewController.swift
-//  Dontpad-iOS
+//  URLSessionJSONRequests
 //
-//  Created by Djorkaeff Alexandre Vilela Pereira on 17/08/2018.
+//  Created by Djorkaeff Alexandre Vilela Pereira on 15/03/18.
 //  Copyright © 2018 Djorkaeff Alexandre Vilela Pereira. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var urlMain = "http://www.dontpad.com/"
+    var str = ""
+    @IBOutlet weak var urlInsert: UITextField!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        urlInsert.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func openTxtArea(_ sender: Any) {
+        getRequest()
+        urlInsert.resignFirstResponder()
     }
-
-
+    
+    func getRequest() -> Void {
+        var urlEntry = urlInsert.text
+        urlEntry = urlMain + urlEntry!
+        
+        guard let url = URL(string: urlEntry!) else { return; }
+        
+        let session = URLSession.shared.dataTask(with: url) { (data, response, poop) in
+            if let data = data {
+                self.str = self.getTextArea(data: data)
+            }
+            
+        }
+        session.resume()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    // This function makes a Regex on a response from URL for recovery the data on a URL
+    func getTextArea(data: Data) -> String {
+        
+        let strData = String(data: data, encoding: String.Encoding.utf8) as String?
+        var string = strData?.replacingOccurrences(of: "<html lang=\"en\">(.|\n)*?<textarea id=\"text\">", with: "", options: .regularExpression, range: nil)
+        
+        string = string?.replacingOccurrences(of: "</textarea>(.|\n)*?</html>", with: "", options: .regularExpression, range: nil)
+        
+        return string!;
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        urlInsert.resignFirstResponder()
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vcDestiny : TextAreaController = segue.destination as! TextAreaController
+        
+        vcDestiny.dataReceived = urlInsert.text!
+        vcDestiny.dataTextArea = str
+    }
 }
-
